be2c3c985d20a5d7960be9f10cc46ad2fb7e308c
Utkarsh <utkarshdbansal@gmail.com>
2021-11-30 11:51:47 +0530

    none

---
+++ trees/src/binaryTree.java
@@ -0,0 +1,45 @@
+
+public class binaryTree {
+    private TreeNode root;
+
+    private class TreeNode{
+        private TreeNode left;
+        private TreeNode right;
+        private int data;
+
+        public TreeNode (int data){
+            this.data = data;
+        }
+    }
+    public void createBinaryTree(){
+        TreeNode first = new TreeNode(1);
+        TreeNode second = new TreeNode(2);
+        TreeNode third = new TreeNode(3);
+        TreeNode fourth = new TreeNode(4);
+        TreeNode fifth = new TreeNode(5);
+        TreeNode sixth = new TreeNode(6);
+        TreeNode seventh = new TreeNode(7);
+        root = first;
+        first.left = second;
+        first.right = third;
+        second.left = fourth;
+        second.right = fifth;
+        third.left = sixth;
+        third.right = seventh;
+    }
+
+    public void preOrder(TreeNode root){
+        if(root == null){
+            return;
+        }
+        System.out.print(root.data+" ");
+        preOrder(root.left);
+        preOrder(root.right);
+    }
+
+    public static void main(String[] args) {
+        binaryTree bt = new binaryTree();
+        bt.createBinaryTree();
+
+    }
+}
